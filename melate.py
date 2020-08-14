a9e269cd46fd5f2be523cbfbb7bb296029fa5118
krenfermo <morakurt@gmail.com>
2020-08-13 23:07:33 -0500

    flask

--- melate.py
+++ melate.py
@@ -13,23 +13,36 @@ def baja_csv():
     data.drop(["NPRODUCTO", "CONCURSO","BOLSA","FECHA"], axis = 1, inplace = True) 
     return data
 
+from flask import Flask
+app = Flask(__name__)
 
+@app.route("/")
+def hello():
+    #https://www.pronosticos.gob.mx/Documentos/Historicos/Melate.csv
+    melate_df = baja_csv()
+    primera=[]
+    for col in melate_df.columns: 
+        primera.append(melate_df[col].value_counts().idxmax())
+        #print("R", melate_df[col].value_counts().idxmax())
+     
 
-#https://www.pronosticos.gob.mx/Documentos/Historicos/Melate.csv
-melate_df = baja_csv()
+    concatenated = pd.concat([melate_df["R1"], melate_df["R2"],melate_df["R3"], melate_df["R4"],melate_df["R5"], melate_df["R6"], melate_df["R7"]])
+    segunda=[]
+    segunda.append(primera)
+    segunda.append("mas comun de todos: "+str(concatenated.value_counts().idxmax()))
+    #print()
+    #print("mas comun de todos",concatenated.value_counts().idxmax())
+    #print()
+    segunda.append(melate_df.value_counts()[:1].index.tolist()[0])
+    #print(melate_df.value_counts()[:1].index.tolist())
+    df = pd.DataFrame (concatenated)
+    segunda.append(df.value_counts()[:7].index.tolist())
+    #print(df.value_counts()[:7].index.tolist())
 
-for col in melate_df.columns: 
-    print("R", melate_df[col].value_counts().idxmax())
- 
+    #print (segunda)
+    return str(segunda)
 
-concatenated = pd.concat([melate_df["R1"], melate_df["R2"],melate_df["R3"], melate_df["R4"],melate_df["R5"], melate_df["R6"], melate_df["R7"]])
-
-
-print()
-print("mas comun de todos",concatenated.value_counts().idxmax())
-print()
-print(melate_df.value_counts()[:1].index.tolist())
-df = pd.DataFrame (concatenated)
-print(df.value_counts()[:7].index.tolist())
+if __name__ == "__main__":
+    app.run(debug=True,port=8000, host='0.0.0.0') 
 
 
